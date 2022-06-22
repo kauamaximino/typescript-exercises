12ab77189125c0af68ce2a94cd9c8d95240731dc
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-22 14:48:58 -0300

    entendendo sobre os tipos básicos no TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-const nome = 'Kauã';
+export function names(
+  name: string,
+  lastName?: string,
+): {
+  name: string;
+  lastName?: string;
+} {
+  return {
+    name,
+    lastName,
+  };
+}
+
+console.log(names('John', 'Doe'));
+
+const squareOf = (x: number) => x * x;
+
+console.log(squareOf(6));
